de2e41b268c964bc5b900252b908f93d5b443ca2
Daniel Zhang <d.zhang200788@gmail.com>
2022-11-05 21:41:54 -0400

    edits to stuff

--- src/components/ListForm/ListForm.tsx
+++ src/components/ListForm/ListForm.tsx
@@ -2,22 +2,24 @@ import React, { FC, useContext } from "react";
 import { useForm } from "react-hook-form";
 import "./ListForm.scss";
 import { ListCtx } from "../ListContext/ListContext";
+import shop from "../../shop.json"
 
 interface ListFormProps {}
 
 const ListForm: FC<ListFormProps> = () => {
   const listContext = useContext(ListCtx);
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit } = useForm();
   const onSubmit = handleSubmit((data) => {
     listContext?.changeList(data.list);
   });
 
   return (
     <div className="ListForm" data-testid="ListForm">
+      <div>Order:</div>
       <form onSubmit={onSubmit}>
         <textarea
           {...register("list")}
-          placeholder="Enter your shopping list, separated by commas"
+          placeholder="Avocado, mangoes, oranges, nectarines, granola, umbrellas, saffron"
         />
         <br />
         <input type="submit" />
--- src/components/UserCanvas/UserCanvas.tsx
+++ src/components/UserCanvas/UserCanvas.tsx
@@ -1,23 +1,64 @@
 import React, { FC, useRef, useEffect, useContext } from "react";
 import "./UserCanvas.scss";
 import shop from "../../shop.json";
-import { ListCtx } from "../ListContext/ListContext";
 
 interface UserCanvasProps {}
 
 const UserCanvas: FC<UserCanvasProps> = () => {
   let canvasRef: any = useRef(null);
-  let context = useContext(ListCtx);
 
   useEffect(() => {
     let canvas = canvasRef.current;
     let ctx = canvas!.getContext("2d");
-  }, [context]);
+    let unit = 49;
+
+    ctx.fillStyle = "#444";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = "16px Verdana";
+    ctx.globalCompositeOperation = "destination-over";
+
+    for (let i = 0; i < 14; i++) {
+      ctx.beginPath();
+      ctx.moveTo(0, i * unit);
+      ctx.lineTo(14 * unit, i * unit);
+      ctx.stroke();
+    }
+    for (let i = 0; i < 14; i++) {
+      ctx.beginPath();
+      ctx.moveTo(i * unit, 0);
+      ctx.lineTo(i * unit, 14 * unit);
+      ctx.stroke();
+    }
+
+    for (let row = 0; row < 13; row++) {
+      for (let column = 0; column < 13; column++) {
+        if (shop.grid[row][column] === 1) {
+          ctx.fillStyle = "#444";
+          ctx.fillRect(column * unit, row * unit, unit, unit);
+        } else if (shop.grid[row][column] > 1) {
+          ctx.fillStyle = "#e22";
+          ctx.fillRect(column * unit, row * unit, unit, unit);
+
+          ctx.globalCompositeOperation = "source-over";
+
+          ctx.fillStyle = "#fff";
+
+          ctx.fillText(
+            `${shop.grid[row][column]}: ${shop.items[shop.grid[row][column]]}`,
+            column * unit + 49 / 2,
+            row * unit + 49 / 2
+          );
+          ctx.globalCompositeOperation = "destination-over";
+        }
+      }
+    }
+  }, []);
 
   return (
-    <div className="UserCanvas" data-testid="UserCanvas">
-      <canvas ref={canvasRef} />
-    </div>
+    // <div className="UserCanvas" data-testid="UserCanvas">
+    <canvas ref={canvasRef} width="686" height="686" />
+    // </div>
   );
 };
 
